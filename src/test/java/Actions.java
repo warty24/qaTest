210ecd0c85847ec0d30decdc69c55ab443061de2
warty <warty24@gmail.com>
2020-02-21 14:26:46 +0200

    To product class added fields: productName, innerId

---
+++ src/test/java/Actions.java
@@ -0,0 +1,166 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import static org.testng.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class Actions {
+        Actions (WebDriver driver) {
+            this.driver = driver;
+        }
+    private WebDriver driver;
+    private String currencyUSD = "USD \u0024";
+    private String currencyEUR = "EUR \u20AC";
+    private String currencyUAH = "UAH \u20B4";
+    int foundProductPositions;
+    private List productList;
+    public char currentCurrency;
+    String[] ar = new String[10];
+    private static final String PRODUCTS = "/html/body/main/section/div/div/section/section";
+
+    //accepts only "USD" | "EUR" | "UAH"
+    protected void changeCurrencyTo(String currency)
+    {
+        Solution.wait.until(ExpectedConditions.elementToBeClickable(By.className("currency-selector")));
+        assert (currency == "USD"||currency == "EUR"| currency == "UAH");
+        WebElement dropDownList = driver.findElement(By.className("currency-selector"));
+        dropDownList.click();
+        if (currency == "USD") {
+            try {
+                Solution.wait.until(ExpectedConditions.elementToBeClickable(By.linkText(currencyUSD)));
+            }
+            catch (TimeoutException e) {
+                driver.navigate().refresh();
+            }
+            finally {
+                WebElement currencyChase = driver.findElement(By.linkText(currencyUSD));
+                currencyChase.click();
+                currentCurrency = '\u0024';
+            }
+        }
+        else if (currency == "EUR"){
+            try {
+                Solution.wait.until(ExpectedConditions.elementToBeClickable(By.linkText(currencyEUR)));
+            }
+            catch (TimeoutException e) {
+                driver.navigate().refresh();
+            }
+            finally {
+                WebElement currencyChase = driver.findElement(By.linkText(currencyEUR));
+                currencyChase.click();
+                currentCurrency = '\u20AC';
+            }
+        }
+        else {
+            try {
+                Solution.wait.until(ExpectedConditions.elementToBeClickable(By.linkText(currencyUAH)));
+            }
+            catch (TimeoutException e) {
+                driver.navigate().refresh();
+            }
+            finally {
+                WebElement currencyChase = driver.findElement(By.linkText(currencyUAH));
+                currencyChase.click();
+                currentCurrency = '\u20B4';
+            }
+        }
+
+    }
+
+    public char getActualCurrency()
+    {
+        try {
+            Solution.wait.until(ExpectedConditions.elementToBeClickable(By.className("currency-selector")));
+        }
+        catch (Exception e)
+        {
+            driver.navigate().refresh();
+        }
+        finally {
+            WebElement dropDownList = driver.findElement(By.className("currency-selector"));
+            if(dropDownList.getText().contains("\u0024")) return '\u0024';
+            if(dropDownList.getText().contains("\u20AC")) return '\u20AC';
+            if(dropDownList.getText().contains("\u20B4")) return '₴';
+            else throw new RuntimeException("Could not get valid currency");
+        }
+
+    }
+
+    protected void findProducts()
+    {
+        Solution.wait.until(ExpectedConditions.elementToBeClickable(By.className("thumbnail-container")));
+        List<WebElement> priceList = driver.findElements(By.className("product-description"));
+        productList = priceList;
+        foundProductPositions = productList.size();
+    }
+
+    protected void isProductsCurrencyActual()
+    {
+        if (currentCurrency != '\u0024'| currentCurrency != '\u20AC'| currentCurrency != '\u20B4')currentCurrency = getActualCurrency();
+        findProducts();
+       WebElement productList = (driver.findElement(By.xpath(PRODUCTS)));
+        System.out.println(currentCurrency + " At all "+ foundProductPositions+ " positions");
+       List foundElements = productList.findElements(By.className("product-price-and-shipping"));
+
+       assertTrue(foundElements.size() == foundProductPositions);
+    }
+
+    protected void sortingCheck() {
+        findProducts();
+        ArrayList<WebElement> products = new ArrayList<WebElement>();
+        currentCurrency = getActualCurrency();
+        ArrayList<WebElement> newList = new ArrayList<WebElement>();
+
+        products.addAll(driver.findElements(By.className("product-price-and-shipping")));
+        for (WebElement i: products
+             ) {
+            newList.add(i.findElement(By.cssSelector("Span")));
+        }
+        for(int i = 1; i < newList.size(); i++) {
+            System.out.println("product #"+i+ " costs "+Float.valueOf(newList.get(i-1).getText().replace(currentCurrency,' ').replace(',','.')));
+            assertTrue(Float.valueOf(newList.get(i-1).getText().replace(currentCurrency,' ').replace(',','.')) >= Float.valueOf(newList.get(i).getText().replace(currentCurrency,' ').replace(',','.')));
+        }
+        System.out.println("product #"+newList.size()+" costs "+ Float.valueOf(newList.get(products.size()-1).getText().replace(currentCurrency,' ').replace(',','.')));
+
+    }
+
+    void discountCheck()
+    {
+        ArrayList<WebElement> products = new ArrayList<WebElement>(driver.findElements(By.cssSelector("article.product-miniature.js-product-miniature")));
+        ArrayList<Product> positions = new ArrayList<Product>();
+        for (int i = 0; i < products.size(); i++) {
+            positions.add(new Product(products.get(i), getActualCurrency()));
+        }
+        for (int i = 0; i < positions.size(); i++)
+        {
+            if(positions.get(i).isDiscounted)
+            {
+                assertTrue((positions.get(i).price - 0.5) <= (((positions.get(i).regularPrice) *(100 -positions.get(i).discount))/100)|((((positions.get(i).regularPrice) *(100 -positions.get(i).discount))/100) <= positions.get(i).price + 0.5));
+                System.out.println("Discount "+ positions.get(i).discount +"% is Actual");
+            }
+        }
+    }
+
+    protected void createFile() {
+        try {
+            File logFile = new File("log.txt");
+            if (logFile.createNewFile()) {
+                System.out.println("Created: " + logFile.getName()+ "\nAt: " + logFile.getPath());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("En error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
---
+++ src/test/java/Solution.java
@@ -0,0 +1,105 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+
+public class Solution {
+    WebDriver driver;
+    static WebDriverWait wait;
+    Actions actions;
+
+   // private char currentCurrency;
+    private static final String DRV_CHROME_V80_Linux = "src/main/resources/chromedriver80";
+    private static final String DRV_CHROME_V81_Linux = "src/main/resources/chromedriver81";
+    private static final String DRV_CHROME_V80_Windows = "src/main/resources/chromedriver80.exe";
+    private static final String DRV_CHROME_V81_Windows = "src/main/resources/chromedriver81.exe";
+
+    private static final String MAIN_PAGE = "http://prestashop-automation.qatestlab.com.ua/ru";
+    private static final String SEARCH_KEYWORDS = "dress";
+    private static final String FOUND_PRODUCT_POSITIONS = "/html/body/main/section/div/div/section/section/div[1]/div/div[1]/p";
+    private static final String SEARCH_FIELD = "/html/body/main/header/div/div/div[1]/div[2]/div/div[2]/form/input[2]";
+    private static final String SORT_BUTTON = "//*[@id=\"js-product-list-top\"]/div[2]/div/div/a";
+    private static final String HIGH_TO_LOW = "/html/body/main/section/div/div/section/section/div[1]/div/div[2]/div/div/div/a[5]";
+
+
+    @BeforeTest
+
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", DRV_CHROME_V80_Windows );
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, 10);
+
+        driver.get(MAIN_PAGE);
+        assertTrue(driver.getCurrentUrl().contains("http://prestashop-automation.qatestlab.com.ua"));
+    }
+
+    @Test
+    public void openMainPage1() {
+        System.out.println("Started Successfully");
+    }
+
+    @Test
+    public void checkCurrency2() {
+        actions.isProductsCurrencyActual();
+    }
+    @Test
+    public void setPriseToUSD3() {
+        actions.changeCurrencyTo("USD");
+        assertEquals(actions.getActualCurrency(),'\u0024');
+        System.out.println("Currency changed to \u0024");
+    }
+    @Test
+    public void searchByKeywords4() {
+        WebElement element = driver.findElement(By.xpath(SEARCH_FIELD));
+        element.clear();
+        element.sendKeys(SEARCH_KEYWORDS);
+        element.submit();
+        assertTrue(driver.getCurrentUrl().contains("search?"));
+        System.out.println("Search for "+ SEARCH_KEYWORDS + " Success!");
+    }
+    @Test
+    public void searchAudition5() {
+        searchByKeywords4();
+        actions.findProducts();
+        System.out.println(driver.findElement(By.xpath(FOUND_PRODUCT_POSITIONS)).getText());
+        assertTrue(driver.findElement(By.xpath(FOUND_PRODUCT_POSITIONS)).getText().contains("Товаров: "+ actions.foundProductPositions));
+    }
+    @Test
+    public void newCheckCurrency6() {
+        actions.isProductsCurrencyActual();
+    }
+    @Test
+    public void productSorting7() {
+        searchByKeywords4();
+        driver.findElement(By.xpath(SORT_BUTTON)).click();
+        driver.findElement(By.xpath(HIGH_TO_LOW)).click();
+        System.out.println("Sorted! (high to low)");
+    }
+    @Test
+    public void sortingCheck8() {
+        productSorting7();
+        actions.sortingCheck();
+    }
+    @Test void discountCheck10() {
+        actions.discountCheck();
+    }
+
+
+    @AfterTest
+    public void finish() {
+       //driver.close();
+    }
+
+}
